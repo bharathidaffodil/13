545c40bcfa50f5da81e1d7b0adf688ce466a410b
bharathidaffodil <47472196+bharathidaffodil@users.noreply.github.com>
2019-02-09 15:52:41 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -7,9 +7,9 @@ void main()
     printf("Enter a number: \n");
     scanf("%d", &num);
     if (num > 0)
-        printf("%d is a positive number \n", num);
+        printf("%d Positive \n", num);
     else if (num < 0)
-        printf("%d is a negative number \n", num);
+        printf("%d Negative \n", num);
     else
-        printf("0 is neither positive nor negative");
+        printf("Zero");
 }
