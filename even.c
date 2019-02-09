2b11d92836f57824a89b2b10e3fcdee4261fc9fa
bharathidaffodil <47472196+bharathidaffodil@users.noreply.github.com>
2019-02-09 16:21:20 +0530

    Update even.c

--- even.c
+++ even.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
-
-
 int main()
-
 {
- 
-  int n;
- if(n>=0)
+ int n;
+ scanf("%d",&n);
+  if(n>=0)
+ {
+  if(n%2==0)
  {
   
- 
- scanf("%d",&n);
-  
- if(n%2==0)
-  
  printf("Even");
+ }
  
   else if(n%2!=0)
+  {
   
- printf("odd");
+ printf("Odd");
+ }
  }
 else
+{
 printf("invalid");
+}
 
-  
   return 0;
 }
